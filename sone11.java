02605537ad112d5983ee1cf42ff076e4dbc65728
soneSEEDAVANH <99340683+soneSEEDAVANH@users.noreply.github.com>
2022-02-11 22:23:31 +0700

    Create sone11.java

---
+++ sone11.java
@@ -0,0 +1,14 @@
+public class sone11 {
+     public static void main(String[] args) {
+         sone e1=new sone();
+         e1. setName(sone);
+         e1. setAge(20);
+         e1. setyaer(3);
+         e1. setprintinofrmattione();
+         e1. setname();
+         e1. setgrade();
+         e1. setmark();
+         e1.setlocation();
+        
+    }
+}
