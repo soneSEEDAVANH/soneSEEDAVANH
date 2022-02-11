793c5904e0dcccedac569ceb2c4dfcc3151b9c75
soneSEEDAVANH <99340683+soneSEEDAVANH@users.noreply.github.com>
2022-02-11 22:22:14 +0700

    Create sone.java

---
+++ sone.java
@@ -0,0 +1,29 @@
+public class sone {
+    public String name;
+    public int age;
+}
+
+    private int getAge() {
+        return age;
+    }
+
+    public int year;
+    public void printinofrmattione(){}
+        class instructor extends person{
+            public void assingn(){
+
+            }
+
+        }
+        class Student extends  person{
+            public int year;
+        }
+        class Course{
+            protected String name;
+            protected double grade;
+        }
+        class Select {
+            protected float mark;
+            protected String location;
+        }
+    }
